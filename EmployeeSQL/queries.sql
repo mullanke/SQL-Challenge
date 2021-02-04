f5b1bd5a5a9a97fa2e1736ef43c6052c42202e87
Kellie Mullan <73853557+mullanke@users.noreply.github.com>
2021-02-04 15:36:06 -0500

    Update queries.sql

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -1,24 +1,18 @@
-/*
-List the following details of each employee:
-employee number, last name, first name, gender, and salary.
-*/
+/* List the following details of each employee:
+employee number, last name, first name, gender, and salary */
 select e.emp_no, e.last_name, e.first_name, e.gender, s.salary
 from employees as e
 inner join salaries as s
 on e.emp_no = s.emp_no;
 
-/*
-List employees who were hired in 1986.
-*/
+/*List employees who were hired in 1986 */
 select *
 from employees
 where extract(year from employees.hire_date) = 1986;
 
-/*
-List the manager of each department with the following information:
+/*List the manager of each department with the following information:
 department number, department name, the manager's employee number,
-last name, first name, and start and end employment dates.
-*/
+last name, first name, and start and end employment dates.*/
 select d.dept_no, d.dept_name,  dm.emp_no, e.last_name, e.first_name, dm.from_date, dm.to_date
 from employees as e
 inner join dept_manager as dm
@@ -26,8 +20,7 @@ on e.emp_no = dm.emp_no
 inner join departments as d
 on dm.dept_no = d.dept_no;
 
-/*
-List the department of each employee with the following information:
+/* List the department of each employee with the following information:
 employee number, last name, first name, and department name.
 */
 select e.emp_no, e.last_name, e.first_name, d.dept_name
